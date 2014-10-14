e8b34c41f6e14f78604f2977f94a94dc5f6c4105
mwq <mu.alex1995@gmail.com>
2014-10-14 20:25:10 +0800

    init

---
+++ src/exp2/LoginAction.java
@@ -0,0 +1,27 @@
+package exp2;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport
+{	
+	private String password;
+	public String login()
+	{
+		if(password.equals("mwq"))
+		{
+			System.out.println("success!");
+			return SUCCESS;
+		}
+		else
+		{
+			this.addFieldError("name", "Wrong Password!");
+			return ERROR;
+		}
+			
+	}
+	public String getPassword(){
+		return password;
+	}
+	public void setPassword(String password){
+		this.password=password;
+	}
+}
